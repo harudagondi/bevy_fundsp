01bc0ff31533ce38858722e08f6887130af7713d
harudagondi <giogdeasis@gmail.com>
2022-09-11 10:01:17 +0800

    remove doctest in lib

--- src/lib.rs
+++ src/lib.rs
@@ -7,40 +7,7 @@
 //!
 //! When using this library, **remember to lower your volume first**!
 //!
-//! The following code registers a DSP graph that just generates noise.
-//!
-//! ```no_run
-//! #![allow(clippy::precedence)]
-//!
-//! use bevy::prelude::*;
-//! use bevy_fundsp::prelude::*;
-//!
-//! fn main() {
-//!     App::new()
-//!         .add_plugins(DefaultPlugins)
-//!         .add_plugin(DspPlugin::default())
-//!         .add_dsp_source(white_noise, SourceType::Dynamic)
-//!         .add_startup_system_to_stage(StartupStage::PostStartup, play_noise)
-//!         .run();
-//! }
-//!
-//! fn white_noise() -> impl AudioUnit32 {
-//!     white() >> split::<U2>() * 0.2
-//! }
-//!
-//! fn play_noise(
-//!     mut assets: ResMut<Assets<DspSource>>,
-//!     dsp_manager: Res<DspManager>,
-//!     mut audio: ResMut<Audio<DspSource>>,
-//! ) {
-//!     let source = dsp_manager
-//!         .get_graph(white_noise)
-//!         .unwrap_or_else(|| panic!("DSP source not found!"));
-//!     audio.play_dsp(assets.as_mut(), source);
-//! }
-//! ```
-//!
-//! When using this library, you may encounter the following error:
+//! Also, you may encounter the following error when using this library:
 //!
 //! ```text no_run
 //! warning: operator precedence can trip the unwary
