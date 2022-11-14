70895af56a21c9ff4d795be09dc67088565b119b
harudagondi <giogdeasis@gmail.com>
2022-11-14 12:33:23 +0800

    Update kira examples

--- examples/kira/interactive.rs
+++ examples/kira/interactive.rs
@@ -1,48 +1,51 @@
-// #![allow(clippy::precedence)]
+#![allow(clippy::precedence)]
 
-// use bevy::prelude::*;
-// use bevy_fundsp::prelude::*;
-// use bevy_kira_audio::*;
+use bevy::prelude::*;
+use bevy_fundsp::prelude::*;
+use bevy_kira_audio::prelude::*;
 
-// fn main() {
-//     App::new()
-//         .add_plugins(DefaultPlugins)
-//         .add_plugin(AudioPlugin)
-//         .add_plugin(DspPlugin)
-//         .add_startup_system(init_dsp)
-//         .add_system(interactive_audio)
-//         .run();
-// }
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(AudioPlugin)
+        .add_plugin(DspPlugin::default())
+        .add_dsp_source(sine_wave, SourceType::Static { duration: 0.5 })
+        .add_dsp_source(triangle_wave, SourceType::Static { duration: 0.5 })
+        .add_system(interactive_audio)
+        .run();
+}
 
-// fn sine_wave() -> impl AudioUnit32 {
-//     // Note is A4
-//     sine_hz(440.0) >> split::<U2>() * 0.2
-// }
+fn sine_wave() -> impl AudioUnit32 {
+    // Note is A4
+    sine_hz(440.0) >> split::<U2>() * 0.2
+}
 
-// fn triangle_wave() -> impl AudioUnit32 {
-//     // Note is G4
-//     triangle_hz(392.0) >> split::<U2>() * 0.2
-// }
+fn triangle_wave() -> impl AudioUnit32 {
+    // Note is G4
+    triangle_hz(392.0) >> split::<U2>() * 0.2
+}
 
-// fn init_dsp(mut dsp_manager: ResMut<DspManager>) {
-//     // length is in seconds
-//     dsp_manager
-//         .add_graph(sine_wave, 5.0)
-//         .add_graph(triangle_wave, 5.0);
-// }
+fn interactive_audio(
+    input: Res<Input<KeyCode>>,
+    mut assets: ResMut<Assets<AudioSource>>,
+    dsp_manager: Res<DspManager>,
+    audio: ResMut<Audio>,
+) {
+    if input.just_pressed(KeyCode::S) {
+        let source = dsp_manager
+            .get_graph(sine_wave)
+            .unwrap_or_else(|| panic!("DSP source not found!"));
+        let audio_source = DefaultBackend::convert_to_audio_source(source.clone());
+        let audio_source = assets.add(audio_source);
+        audio.play(audio_source);
+    }
 
-// fn interactive_audio(input: Res<Input<KeyCode>>, dsp_assets: Res<DspAssets>, audio: Res<Audio>) {
-//     if input.just_pressed(KeyCode::S) {
-//         audio.play_looped(dsp_assets.graph(&sine_wave));
-//     }
-
-//     if input.just_pressed(KeyCode::T) {
-//         audio.play_looped(dsp_assets.graph(&triangle_wave));
-//     }
-
-//     if input.just_pressed(KeyCode::P) {
-//         audio.stop();
-//     }
-// }
-
-fn main() {}
+    if input.just_pressed(KeyCode::T) {
+        let source = dsp_manager
+            .get_graph(triangle_wave)
+            .unwrap_or_else(|| panic!("DSP source not found!"));
+        let audio_source = DefaultBackend::convert_to_audio_source(source.clone());
+        let audio_source = assets.add(audio_source);
+        audio.play(audio_source);
+    }
+}
--- examples/kira/noise.rs
+++ examples/kira/noise.rs
@@ -1,30 +1,32 @@
-// #![allow(clippy::precedence)]
+#![allow(clippy::precedence)]
 
-// use bevy::prelude::*;
-// use bevy_fundsp::prelude::*;
-// use bevy_kira_audio::*;
+use bevy::prelude::*;
+use bevy_fundsp::prelude::*;
+use bevy_kira_audio::prelude::*;
 
-// fn main() {
-//     App::new()
-//         .add_plugins(DefaultPlugins)
-//         .add_plugin(AudioPlugin)
-//         .add_plugin(DspPlugin)
-//         .add_startup_system(init_dsp)
-//         .add_startup_system_to_stage(StartupStage::PostStartup, play_noise)
-//         .run();
-// }
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(AudioPlugin)
+        .add_plugin(DspPlugin::default())
+        .add_dsp_source(white_noise, SourceType::Static { duration: 60.0 })
+        .add_startup_system_to_stage(StartupStage::PostStartup, play_noise)
+        .run();
+}
 
-// fn white_noise() -> impl AudioUnit32 {
-//     white() >> split::<U2>() * 0.2
-// }
+fn white_noise() -> impl AudioUnit32 {
+    white() >> split::<U2>() * 0.2
+}
 
-// fn init_dsp(mut dsp_manager: NonSendMut<DspManager>) {
-//     dsp_manager.add_graph(white_noise, 30.0); // length is in seconds
-// }
-
-// fn play_noise(dsp_assets: Res<DspAssets>, audio: Res<Audio>) {
-//     let white_noise = dsp_assets.get_graph(white_noise).unwrap();
-//     audio.play_looped(white_noise.clone());
-// }
-
-fn main() {}
+fn play_noise(
+    mut assets: ResMut<Assets<AudioSource>>,
+    dsp_manager: Res<DspManager>,
+    audio: ResMut<Audio>,
+) {
+    let source = dsp_manager
+        .get_graph(white_noise)
+        .unwrap_or_else(|| panic!("DSP source not found!"));
+    let audio_source = DefaultBackend::convert_to_audio_source(source.clone());
+    let audio_source = assets.add(audio_source);
+    audio.play(audio_source);
+}
